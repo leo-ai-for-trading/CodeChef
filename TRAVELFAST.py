1eca0f9681a5e15ab92831c4cda0779b351c0669
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-03-15 18:28:45 +0100

    Delete TRAVELFAST.py

--- TRAVELFAST.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for tc in range(T):
-	# Read integers a and b.
-    (a,b) = map(int,input().split(' '))
-    ans = a - b
-    print('same') if ans == 0 else print('bike') if ans < 0 else print('car') 
-	 
