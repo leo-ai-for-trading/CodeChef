fee9b6351420e9dc4160c24dc15cdd5f50238fec
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-04-06 20:25:07 +0200

    Update FLIPS.py

--- Contest/FLIPS.py
+++ Contest/FLIPS.py
@@ -1,4 +1,8 @@
-
+"""
+- if 1 are not adjiacent to each other we can solve it in just one move
+- if they are all zeros no move required
+- if the ones are adjency we need to make 2 moves
+"""
 import sys
 t = int(sys.stdin.readline())
 for _ in range(t):
