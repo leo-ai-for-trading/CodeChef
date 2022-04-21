096ce6b36a2f2c0c35a03d87c0452e5c172f692c
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-04-21 21:34:47 +0200

    Update CHEF FIND XOR BEAUTIFUL.py

--- Contest/CHEF FIND XOR BEAUTIFUL.py
+++ Contest/CHEF FIND XOR BEAUTIFUL.py
@@ -1,4 +1,15 @@
 from collections import Counter
+"""
+- a_i XOR a_j) AND x = 0
+implies that (a_i AND x) XOR (a_j AND x) = 0
+that implies A_i + A_j = 0
+A_i == A_j
+modify the array of the input to become A_i = a_i AND x
+- find the counter of similar number
+- map the array if you are in c++ to gent map<int,int>, where the first element is the element and the second is the frequency
+- ans = sum(freq of i times freq of i element)
+- iterate over the map and ans = sum of (p1 * second)^2
+"""
 k=int(input())
 while k:
     k-=1
