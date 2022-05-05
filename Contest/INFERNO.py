5f99849ce67ac2b681595b01a0f69a992cacc680
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-05-05 21:05:42 +0200

    Create INFERNO.py

---
+++ Contest/INFERNO.py
@@ -0,0 +1,15 @@
+#arrotondare
+
+T = int(input())
+import math
+for _ in range(T):
+    n,x = map(int,input().split())
+    A = list(map(int,input().split()))
+    multi_target_time = max(A)
+    single_target_time = 0
+    for i in A:
+        if i <=x:
+            single_target_time += 1 
+        else:
+            single_target_time += math.ceil(i/x)
+    print(min(multi_target_time,single_target_time))
